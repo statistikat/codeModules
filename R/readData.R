0957e1f3edc6d251911a4d29ca59741f3e0bea02
Gregor de Cillia <de.cillia.gregor@gmail.com>
2018-03-27 15:33:23 +0200

    New parameter for readData

--- R/readData.R
+++ R/readData.R
@@ -8,6 +8,8 @@
 #' @param input,output,session Standard module parameters.
 #' @param path A reactive value representing the path of the file to be read.
 #' @param callback A function that will be called in case of unsupported formats. Should have no arguments.
+#' @param symbol Optional: A symbol to use in the import code in place of the path. This is useful if the
+#'               path has previously been saved as a variable.
 #' @return A reactive string representing the code to read the dataset.
 #'
 #' @examples
@@ -26,7 +28,7 @@
 #' )
 #'
 #' }
-readData <- function(input, output, session, path, callback = function(){}){
+readData <- function(input, output, session, path, callback = function(){}, symbol = NULL){
   fileType <- reactive({
     ending <- tools::file_ext(req(path()))
     switch(ending, txt = "csv", xls = "xlsx", rdata = "Rdata", ending)
@@ -47,7 +49,10 @@ readData <- function(input, output, session, path, callback = function(){}){
   })
 
   code <- eventReactive(input$read, {
-    fp <- list(file = path())
+    if (is.null(symbol))
+      fp <- list(file = path())
+    else
+      fp <- list(file = as.symbol(symbol))
 
     paste0(
       switch(
